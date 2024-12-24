cb21bf929c4d8e0599c2e3c91d07f2bf9e0fb96a
xbrymo06 <xbrymo06@gmail.com>
2024-12-24 06:37:48 +0000

    first commit

---
+++ lib/audio_component.dart
@@ -0,0 +1,16 @@
+library audio_component;
+export './extension/format_extension.dart';
+export './widgets/album_art.dart';
+export './widgets/audio_appbar.dart';
+export './widgets/song_info.dart';
+export './widgets/volume.dart';
+//! Get [AlbumArt()]
+//! Get [AudioAppBar()]
+//! Get [SongInfo()]
+//! Get [Volume()]
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
